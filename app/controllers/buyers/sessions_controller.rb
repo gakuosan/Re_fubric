b920bbfe1e14d92e98908bb3eb68948d2e05800c
gakuchan <gakuo19831201@gmail.com>
2019-07-22 21:54:52 +0900

    Controller名の修正

---
+++ app/controllers/buyers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class Buyers::SessionsController< ApplicationController
+  #skip_before_action :logged_in_buyer, only: %i[new create]
+
+  def new
+  end
+
+  #def create
+    buyer = Buyer.find_by(email: params[:session][:email])
+    if buyer && buyer.authenticate(params[:session][:password])
+      if buyer.activated?
+        log_in buyer
+        params[:session][:remember_me] == '1' ? remember(buyer) : forget(buyer)
+        redirect_back_or root_path
+      else
+        message = 'アカウントがアクティブになっていません。'
+        message += '招待メールを確認してください'
+        redirect_to login_path, alert: message
+      end
+    else
+      flash.now[:alert] = 'メールアドレスまたはパスワードが正しくありません'
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to log_in_path
+  end
--- app/controllers/sellers/sessions_controller.rb
+++ app/controllers/sellers/sessions_controller.rb
@@ -1,20 +1,20 @@
-class SessionController< ApplicationController
-  #skip_before_action :logged_in_buyer, only: %i[new create]
+class Sellers::SessionsController< ApplicationController
+  skip_before_action :logged_in_seller, only: %i[new create]
 
   def new
   end
 
-  #def create
-    buyer = Buyer.find_by(email: params[:session][:email])
-    if buyer && buyer.authenticate(params[:session][:password])
-      if buyer.activated?
-        log_in buyer
-        params[:session][:remember_me] == '1' ? remember(buyer) : forget(buyer)
+  def create
+    seller = Seller.find_by(email: params[:session][:email])
+    if seller && seller.authenticate(params[:session][:password])
+      if seller.activated?
+        log_in seller
+        params[:session][:remember_me] == '1' ? remember(seller) : forget(seller)
         redirect_back_or root_path
       else
         message = 'アカウントがアクティブになっていません。'
         message += '招待メールを確認してください'
-        redirect_to login_path, alert: message
+        redirect_to log_in_path, alert: message
       end
     else
       flash.now[:alert] = 'メールアドレスまたはパスワードが正しくありません'
